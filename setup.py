3678022f85025d2df580d35654bf10c983e87be7
Eduard Dik <oddshellnick.programming@gmail.com>
2025-02-05 13:18:25 +0700

    1.3.13 release commit

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def get_description() -> str:
 
 setup(
 		name="PyWebScraping",
-		version="1.3.12",
+		version="1.3.13",
 		author="oddshellnick",
 		author_email="oddshellnick.programming@gmail.com",
 		description=get_description(),
